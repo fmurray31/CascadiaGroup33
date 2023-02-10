02829ef0d7fadf3c915c99849df2ddfddeed0b2d
jrena7 <102364482+jrena7@users.noreply.github.com>
2023-02-10 21:10:54 +0000

    Update loops into method
    loop into method in setup file

--- Cascadia.java
+++ Cascadia.java
@@ -33,77 +33,12 @@ public class Cascadia {
         tiles.drawCentralTiles();
         tiles.displayCentralTiles();
 
-        for (int i = 1; i < numUsers; i++) {
-
-        }
-
-
-        //Do While loop, prompts the user if they would like to play
-        do {
-            System.out.println("\nPlayer " + playerArray[i].getUserName()+ "'s" + " Habitat:\n");
-            playerArray[i].printMap(playerArray[i]);
-            System.out.println("\n\n[1] View next player's map");
-            System.out.println("[2] Exit program");
-            inPlay = in.nextInt();
-        } while (inPlay != 1 && inPlay != 2);
-
-        //If the user would like to play....
-        if (inPlay == 1) {
-            int i = 0;
-            while (i < numUsers) {
-                playerArray[i].addHabitatToMap(testHab, 1, 1);
-                playerArray[i].printMap(playerArray[i]);
-
-                System.out.println("[1] View next player's map");
-                System.out.println("[2] Exit program");
-                inPlay = in.nextInt();
-
-                if (inPlay != 1 && inPlay != 2) {
-                    System.out.println("Invalid choice!");
-                    System.out.println("[1] View next player's map");
-                    System.out.println("[2] Exit program");
-                    inPlay = in.nextInt();
-                } else if (inPlay == 1) {
-                    i++;
-                } else if (inPlay == 2) {
-                    System.exit(0);
-                }
-            }
             /*
             // initialise all new habitats in map to "blank" habitats, add functionality to print blank habitats in terrainToAscii and animalToAscii
             playerArray[0].addHabitatToMap(testHab, 1, 1);
             System.out.println(playerArray[0].printSingleTile(playerArray[0], 0, 0));
             playerArray[0].printMap(playerArray[0]);
              */
-
-        //Incomplete, supposed to loop through the list of players and their maps 1 at a time, prompts user if they would like ot continue
-        int i = 0;
-        while (i < numUsers) {
-            playerArray[i].addHabitatToMap(testHab, 1, 1);
-            playerArray[i].printMap(playerArray[i]);
-
-            System.out.println("[1] View next player's map");
-            System.out.println("[2] Exit program");
-            inPlay = in.nextInt();
-
-            if (inPlay != 1 && inPlay != 2) {
-                System.out.println("Invalid choice!");
-                System.out.println("[1] View next player's map");
-                System.out.println("[2] Exit program");
-                inPlay = in.nextInt();
-            } else if (inPlay == 1) {
-                i++;
-            } else if (inPlay == 2) {
-                System.exit(0);
-            } else if (inPlay == 2) {
-                System.exit(0);
-            }
-        }
-
-        //initialise all new habitats in map to "blank" habitats, add functionality to print blank habitats in terrainToAscii and animalToAscii
-        //playerArray[0].addHabitatToMap(testHab, 1, 1);
-        //System.out.println(playerArray[0].printSingleTile(playerArray[0], 0, 0));
-        //playerArray[0].printMap(playerArray[0]);
     }
 }
 
--- SetupInput.java
+++ SetupInput.java
@@ -47,4 +47,64 @@ public class SetupInput {
         System.out.println("\n\n");
         return players;
     }
+
+    //Printing and Displaying map of users, with user prompts
+    public void userPrompts (Player []playerArray, int numUsers, HabitatTiles testHab) {
+        Scanner in = new Scanner(System.in);
+        int userInput = 0;
+        int i = 0;
+        System.out.println("Player " + playerArray[i].getUserName() + "'s" + " Habitat");
+
+        while (userInput == 0) {
+            try {
+                int num = Integer.parseInt(in.nextLine());
+
+                if (num > 2) {
+                    System.out.println("Choice must be either 1 or 2");
+                } else if (num < 1) {
+                    System.out.println("Choice must be either 1 or 2");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Value entered must be either integer 1 or 2");
+            }
+
+            playerArray[i].addHabitatToMap(testHab, 1, 1);
+            playerArray[i].printMap(playerArray[i]);
+
+            System.out.println("[1] View next player's map");
+            System.out.println("[2] Exit program\n\n");
+            userInput = in.nextInt();
+
+            /*
+            if (userInput != 1 && userInput != 2) {
+                System.out.println("Invalid choice!");
+                System.out.println("[1] View next player's map");
+                System.out.println("[2] Exit program");
+                userInput = in.nextInt();
+            } else if (userInput == 1) {
+                i++;
+            } else if (userInput == 2) {
+                System.exit(0);
+            }
+             */
+        }
+    }
 }
+
+/*
+        while (numUsers == 0) {
+            try {
+                int num = Integer.parseInt(in.nextLine());
+
+                if (num > 4) {
+                    System.out.println("Number of players cannot be greater than 4");
+                } else if (num < 2) {
+                    System.out.println("Number of players cannot be less than 2");
+                } else {
+                    numUsers = num;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Value entered must be in integer form, i.e. 2, 3, 4");
+            }
+        }
+ */
