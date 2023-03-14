90d170e78297984ef9d788711189f3f47862dec8
fmurray31 <finner31@gmail.com>
2023-03-14 18:17:49 +0000

    bearB and foxA scoring created but untested

--- Score.java
+++ Score.java
@@ -18,7 +18,10 @@ public class Score {
                 case "BearB": bearBScore(); break;
                 case "BearC": bearCScore(); break;
 
-                case "FoxA": break;
+                case "FoxA": foxAScore(); break;
+                case "FoxB": foxBScore(); break;
+
+                default: throw new IllegalArgumentException("Invalid score card passed to scorePlayer: " + scoreCards.get(i).getCardTitle());
             }
         }
     }
@@ -26,15 +29,20 @@ public class Score {
 
 
     private void bearAScore () {
-        int coordinates[][] = new int[player.getMaxMap()][player.getMaxMap()];
+        int[][] coordinates = new int[player.getMaxMap()][player.getMaxMap()];
+        int[] outputCoordinates;
         int pairCount = 0;
 
         for (int i=0; i< player.getMaxMap(); i++) {
             for (int j=0; j<player.getMaxMap(); j++) {
                 if (player.getPlayerMap()[i][j].isOccupied() && player.getPlayerMap()[i][j].getCreature1().equals("Bear")) {
                     if (coordinates[i][j] == 0) {
-                        if (adjacentAnimal("Bear", coordinates, i, j)) {
+                        coordinates[i][j] = 1;
+
+                        outputCoordinates = adjacentAnimal("Bear", coordinates, i, j);
+                        if (outputCoordinates[0] != -1) {
                             pairCount++;
+                            coordinates[outputCoordinates[0]][outputCoordinates[1]] = 1;
                         }
                     }
                 }
@@ -50,40 +58,136 @@ public class Score {
             default: player.addScore(27); break;
         }
     }
-    private void bearBScore () {}
+    private void bearBScore () {
+        int[][] coordinates = new int[player.getMaxMap()][player.getMaxMap()];
+        int[] outputCoordinates;
+        int lineCount = 0;
+
+        for (int i=0; i< player.getMaxMap(); i++) {
+            for (int j=0; j<player.getMaxMap(); j++) {
+                if (player.getPlayerMap()[i][j].isOccupied() && player.getPlayerMap()[i][j].getCreature1().equals("Bear")) {
+                    if (coordinates[i][j] == 0) {
+                        coordinates[i][j] = 1;
+                        int numAdjacent = 1;
+
+                        outputCoordinates = adjacentAnimal("Bear", coordinates, i, j);
+
+                        while (outputCoordinates[0] != -1) {
+                            coordinates[outputCoordinates[0]][outputCoordinates[1]] = 1;
+                            numAdjacent++;
+                            outputCoordinates = adjacentAnimal("Bear", coordinates, outputCoordinates[0], outputCoordinates[2]);
+                        }
+                        if (numAdjacent >= 3) {
+                            lineCount++;
+                        }
+                    }
+                }
+            }
+        }
+        player.addScore(lineCount*10);
+    }
     private  void bearCScore() {}
 
-    // method which takes the string form of an animal, two arraylists representing x and y coordinates, and two ints i and j, which represent the current coordinates to be checked
-    // the method returns a boolean which represents whether a tile adjacent to the provided coordinates contains an animal matching the provided string
-    // the method also updates the provided arraylists with the coordinates of the found animal
-    private boolean adjacentAnimal(String animal, int[][] coordinates, int i, int j) {
+    private void foxAScore() {
+        int[] outputCoordinates;
+        int[][] placeHolder = new int[player.getMaxMap()][player.getMaxMap()];
+
+        for (int i=0; i< player.getMaxMap(); i++) {
+            for (int j=0; j< player.getMaxMap(); j++) {
+                if (player.getPlayerMap()[i][j].isOccupied() && player.getPlayerMap()[i][j].getCreature1().equals("Fox")) {
+                    int uniqueCount = 0;
+
+                    outputCoordinates = adjacentAnimal("Bear", placeHolder, i, j);
+                    if (outputCoordinates[0] != -1) {
+                        uniqueCount++;
+                    }
+
+                    outputCoordinates = adjacentAnimal("Fox", placeHolder, i, j);
+                    if (outputCoordinates[0] != -1) {
+                        uniqueCount++;
+                    }
+
+                    outputCoordinates = adjacentAnimal("Elk", placeHolder, i, j);
+                    if (outputCoordinates[0] != -1) {
+                        uniqueCount++;
+                    }
+
+                    outputCoordinates = adjacentAnimal("Hawk", placeHolder, i, j);
+                    if (outputCoordinates[0] != -1) {
+                        uniqueCount++;
+                    }
+
+                    outputCoordinates = adjacentAnimal("Salmon", placeHolder, i, j);
+                    if (outputCoordinates[0] != -1) {
+                        uniqueCount++;
+                    }
+
+                    player.addScore(uniqueCount);
+                }
+            }
+        }
+    }
+
+    private void foxBScore() {
+        int[] outputCoordinates;
+
+        for (int i=0; i< player.getMaxMap(); i++) {
+            for (int j=0; j< player.getMaxMap(); j++) {
+                
+            }
+        }
+    }
+
+    // this method takes the name of an animal, a 2d array of coordinates and two integers representing a current location as an argument. It checks each adjacent tile to see
+    // if it contains the provided animal type and has not already been marked as counted, and if it is successful it returns the coordinates of that copy. Otherwise,
+    // it returns [-1, -1]
+    private int[] adjacentAnimal(String animal, int[][] coordinates, int i, int j) {
+        int[] output = new int[2];
 
         // tiles to the left and right are always at the same coordinates, whether x is even or not
         if (player.getPlayerMap()[i][j - 1].isOccupied() && player.getPlayerMap()[i][j - 1].getCreature1().equals(animal)) {
-            coordinates[i][j-1] = 1;
-            return true;
+            if (coordinates[i][j-1] == 0) {
+                output[0] = i;
+                output[1] = j-1;
+                return output;
+            }
         } else if (player.getPlayerMap()[i][j + 1].isOccupied() && player.getPlayerMap()[i][j + 1].getCreature1().equals(animal)) {
-            coordinates[i][j+1] = 1;
-            return true;
+            if (coordinates[i][j+1] == 0) {
+                output[0] = i;
+                output[1] = j+1;
+                return output;
+            }
         }
 
         // checking adjacent tiles to an even x coordinate
         if (i % 2 == 0) {
             // tiles below x
             if (player.getPlayerMap()[i - 1][j].isOccupied() && player.getPlayerMap()[i - 1][j].getCreature1().equals(animal)) {
-                coordinates[i-1][j] = 1;
-                return true;
+                if (coordinates[i-1][j] == 0) {
+                    output[0] = i-1;
+                    output[1] = j;
+                    return output;
+                }
             } if (player.getPlayerMap()[i - 1][j + 1].isOccupied() && player.getPlayerMap()[i - 1][j + 1].getCreature1().equals(animal)) {
-                coordinates[i-1][j+1] = 1;
-                return true;
+                if (coordinates[i-1][j+1] == 0) {
+                    output[0] = i-1;
+                    output[1] = j+1;
+                    return output;
+                }
             }
             // tiles above x
             if (player.getPlayerMap()[i + 1][j].isOccupied() && player.getPlayerMap()[i+1][j].getCreature1().equals(animal)) {
-                coordinates[i+1][j] = 1;
-                return true;
+                if (coordinates[i+1][j] == 0) {
+                    output[0] = i+1;
+                    output[1] = j;
+                    return output;
+                }
             } if (player.getPlayerMap()[i + 1][j+1].isOccupied() && player.getPlayerMap()[i+1][j+1].getCreature1().equals(animal)) {
-                coordinates[i+1][j+1] = 1;
-                return true;
+                if (coordinates[i+1][j+1] == 0) {
+                    output[0] = i+1;
+                    output[1] = j+1;
+                    return output;
+                }
             }
         }
 
@@ -91,22 +195,37 @@ public class Score {
         else {
             // checking tiles below x
             if (player.getPlayerMap()[i - 1][j-1].isOccupied() && player.getPlayerMap()[i - 1][j-1].getCreature1().equals(animal)) {
-                coordinates[i-1][j-1] = 1;
-                return true;
+                if (coordinates[i-1][j-1] == 0) {
+                    output[0] = i-1;
+                    output[1] = j-1;
+                    return output;
+                }
             } if (player.getPlayerMap()[i - 1][j].isOccupied() && player.getPlayerMap()[i - 1][j].getCreature1().equals(animal)) {
-                coordinates[i-1][j] = 1;
-                return true;
+                if (coordinates[i-1][j] == 0) {
+                    output[0] = i-1;
+                    output[1] = j;
+                    return output;
+                }
             }
 
             // tiles above x
             if (player.getPlayerMap()[i + 1][j-1].isOccupied() && player.getPlayerMap()[i + 1][j-1].getCreature1().equals(animal)) {
-                coordinates[i+1][j-1] = 1;
-                return true;
+                if (coordinates[i+1][j-1] == 0) {
+                    output[0] = i+1;
+                    output[1] = j-1;
+                    return output;
+                }
             } if (player.getPlayerMap()[i + 1][j].isOccupied() && player.getPlayerMap()[i + 1][j].getCreature1().equals(animal)) {
-                coordinates[i+1][j] = 1;
-                return true;
+                if (coordinates[i+1][j] == 0) {
+                    output[0] = i+1;
+                    output[1] = j;
+                    return output;
+                }
             }
         }
-        return false;
+        output[0] = -1;
+        output[1] = -1;
+
+        return output;
     }
 }
